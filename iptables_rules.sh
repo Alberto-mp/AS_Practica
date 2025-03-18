39369cacafd95f7139f3a20496183d1ae97c154b
Alberto-mp <alberto.munozpina@alum.uca.es>
2025-03-18 12:33:17 +0100

    iptables configurado

--- iptables_rules.sh
+++ iptables_rules.sh
@@ -10,10 +10,19 @@ iptables-legacy -X FORWARD
 # 🔥 BLOQUEAR TODO EL TRÁFICO POR DEFECTO
 iptables-legacy -P FORWARD DROP
 
-# 🔓 PERMITIR tráfico entre services_net (172.20.0.0/24) y production_net (172.30.0.0/24)
-iptables-legacy -A FORWARD -s 172.20.0.0/24 -d 172.30.0.0/24 -j ACCEPT
-iptables-legacy -A FORWARD -s 172.30.0.0/24 -d 172.20.0.0/24 -j ACCEPT
-
-# ❌ BLOQUEAR tráfico entre development_net (172.40.0.0/24) y production_net (172.30.0.0/24)
-iptables-legacy -A FORWARD -s 172.40.0.0/24 -d 172.30.0.0/24 -j DROP
+# 🔒 BLOQUEAR tráfico entre production_net (172.30.0.0/24) y development_net (172.40.0.0/24)
 iptables-legacy -A FORWARD -s 172.30.0.0/24 -d 172.40.0.0/24 -j DROP
+iptables-legacy -A FORWARD -s 172.40.0.0/24 -d 172.30.0.0/24 -j DROP
+
+# 🔓 PERMITIR tráfico entre services_net (172.20.0.0/24) y production_net (172.30.0.0/24)
+# Pero solo para PostgreSQL (puerto 5432) y rsync (puerto 873)
+
+# Permitir tráfico de PostgreSQL desde Production hacia Services
+iptables-legacy -A FORWARD -s 172.30.0.0/24 -d 172.20.0.0/24 -p tcp --dport 5432 -j ACCEPT
+
+# Permitir tráfico de PostgreSQL desde Services hacia Production (en caso de que sea necesario)
+iptables-legacy -A FORWARD -s 172.20.0.0/24 -d 172.30.0.0/24 -p tcp --dport 5432 -j ACCEPT
+
+# Permitir tráfico de rsync (puerto 873) entre Production y Services
+iptables-legacy -A FORWARD -s 172.30.0.0/24 -d 172.20.0.0/24 -p tcp --dport 873 -j ACCEPT
+iptables-legacy -A FORWARD -s 172.20.0.0/24 -d 172.30.0.0/24 -p tcp --dport 873 -j ACCEPT
